33be2236e3a1fae51274a306fb222afd1ea8e59d
onyriv <onyriv@yahoo.com>
2021-03-09 09:38:20 +0000

    first commit

---
+++ ansible-nginx/main.tf
@@ -0,0 +1,66 @@
+locals {
+  vpc_id           = "vpc-8d5b03e5"
+  subnet_id        = "subnet-853a18ec"
+  ssh_user         = "ubuntu"
+  key_name         = "onome-ec2-key"
+  private_key_path = "./onome-ec2-key.pem"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_security_group" "nginx" {
+  name = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami = "ami-005383956f2e5fb96"
+  subnet_id                   = local.subnet_id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.nginx.id]
+  #security_groups = [aws_security_group.nginx.name]
+  key_name        = local.key_name
+
+  # mandatory so ssh is ready to accept connection
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} nginx.yaml"
+  }
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
